7143228b2559c9a1e61196de9677ee711566173f
hangshen <1425646743@qq.com>
2019-08-19 21:01:10 +0800

    付宇航8/19.20：58提交成功

--- src/main/java/org/lanqiao/service/TypeServiceImpl.java
+++ src/main/java/org/lanqiao/service/TypeServiceImpl.java
@@ -1,9 +1,12 @@
 package org.lanqiao.service;
 
 import org.lanqiao.entity.Type;
+import org.lanqiao.entity.Type;
 import org.lanqiao.mapper.TypeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TypeServiceImpl implements TypeService {
@@ -11,12 +14,33 @@ public class TypeServiceImpl implements TypeService {
     TypeMapper typeMapper;
 
     @Override
-    public Type checkType(Type type){
+    public Type checkType(Type type) {
         return typeMapper.checkType(type);
     }
 
     @Override
     public int insertType(Type type){
         return typeMapper.insertType(type);
+    }
+
+    @Override
+    public List<Type> getAllType(){
+        return typeMapper.getAllType();
+    }
+
+    @Override
+    public List<Type> findType(String typeName){
+        typeName = "%" + typeName + "%";
+        return typeMapper.findType(typeName);
+    }
+
+    @Override
+    public int deleteType(int typeId){
+        return typeMapper.deleteType(typeId);
+    }
+
+    @Override
+    public int updateType(Type type){
+        return typeMapper.updateType(type);
     }
 }
